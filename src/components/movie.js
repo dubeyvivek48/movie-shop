4966c1f2fafea903677dba7559b8bd40a308f318
dubeyvivek48 <dubeyvivek48@gmail.com>
2018-10-17 01:40:39 +0530

    UI Changes

--- src/components/movie.js
+++ src/components/movie.js
@@ -6,7 +6,7 @@ const Movie=(props)=>{
            <div className="card shadow" >
                 <img className="card-img-top img-responsive" src={props.info.Poster=="N/A"?Img:props.info.Poster} alt="Card image cap" />
                 <div className="card-body">
-                    <h5 className="card-title">{props.info.Title}</h5>
+                    <h5 className="card-title">{props.info.Title}</h5><hr></hr>
                     <p className="card-text"> Type: {props.info.Type}</p>
                     <p className="card-text"> Price: ${props.info.imdbID.substr(6, 3)}</p>
                     <a href="#" className="btn btn-primary shadow"> <i className="fas fa-shopping-cart"></i> Add to cart</a>
