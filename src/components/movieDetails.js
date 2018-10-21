d0585a1a54ca7ca81a51720c832aa06939d23aae
dubeyvivek48 <dubeyvivek48@gmail.com>
2018-10-21 15:43:49 +0530

    adding UI for Movies Detail

--- src/components/movieDetails.js
+++ src/components/movieDetails.js
@@ -14,12 +14,13 @@ class MovieDetails extends Component {
                <div class="row">
                <div class="col-md-4 col-sm-6 col-xs-12 mt-2 mb-2 ">
                <div class="card shadow">
-               <img class="card-img-top img-responsive" src={this.props.movieDetails.Poster} alt="Card image cap" />
+               <img class="card-img-top mobiDetails img-responsive" src={this.props.movieDetails.Poster} alt="Card image cap" />
                <div class="card-body">
               
                <hr />
                <a class="btn buy cart-link btn-primary shadow"><i class="fas fa-shopping-cart"></i> Add to cart</a>
                 <a class="btn btn-primary cart-link shadow"><i class="fas fa-bolt"></i>  Buy  </a>
+                <br></br>
                 </div>
                 </div>
                 </div>
@@ -28,10 +29,28 @@ class MovieDetails extends Component {
                 <div class="col-md-8 col-sm-6 col-xs-12 mt-2 mb-2 ">
                  <div class="card shadow">
                     <div class="card-body">                             
-                        <h5 class="card-title "> {"Title : "+this.props.movieDetails.Title} </h5>
+                        <strong class="card-title "> {"Title : "+this.props.movieDetails.Title} </strong>
                         <hr />
                         <p> {"Actors: : "+this.props.movieDetails.Actors} </p>
                         <hr />
+                        <p> {"Genre : "+this.props.movieDetails.Genre} </p>
+                        <hr />
+                        <p> {"Director : "+this.props.movieDetails.Director} </p>
+                        <hr />
+                        <p> {"Ratings : "+this.props.movieDetails.imdbRating} </p>
+                        <hr />
+                        <p> {"Year : "+this.props.movieDetails.Year} </p>
+                </div>
+                </div>                
+                </div>
+                </div>
+                <div className="row">
+                        <div className="col-md-12">                      
+                        <div class="card shadow">
+                    <div class="card-body">    
+                        <p> {"Plot : "+this.props.movieDetails.Plot} </p>
+                        <hr />
+
                         <p> {"Awards : "+this.props.movieDetails.Awards} </p>
                         <hr />
                         <p> {"BoxOffice : "+this.props.movieDetails.BoxOffice} </p>
@@ -40,10 +59,8 @@ class MovieDetails extends Component {
                         <hr />
                         <p> {"DVD : "+this.props.movieDetails.DVD} </p>
                         <hr />
-                        <p> {"Director : "+this.props.movieDetails.Director} </p>
-                        <hr />
-                        <p> {"Genre : "+this.props.movieDetails.Genre} </p>
-                        <hr />
+                       
+                        
                         <p> {"Language : "+this.props.movieDetails.Language} </p>
                         <hr />
                         <p> {"Metascore : "+this.props.movieDetails.Metascore} </p>
@@ -52,8 +69,7 @@ class MovieDetails extends Component {
                         <hr />
                         <p> {"Rated: "+this.props.movieDetails.Rated} </p>
                         <hr />
-                        <p> {"Ratings : "+this.props.movieDetails.imdbRating} </p>
-                        <hr />
+                        
                         <p> {"Released : "+this.props.movieDetails.Released} </p>
                         <hr />
                         <p> {"Response : "+this.props.movieDetails.Response} </p>
@@ -62,17 +78,13 @@ class MovieDetails extends Component {
                         <hr />
                         <p> {"Type : "+this.props.movieDetails.Type} </p>
                         <hr />
-                        <p> {"Website : "+this.props.movieDetails.Website} </p>
+                        <p>Website : <a href={this.props.movieDetails.Website} target="_blank">{" "+this.props.movieDetails.Website} </a></p>
                         <hr />
                         <p> {"Writer: "+this.props.movieDetails.Writer} </p>
                         <hr />
-                        <p> {"Year : "+this.props.movieDetails.Year} </p>
-                        <hr />
                         <p> {"imdbVotes : "+this.props.movieDetails.imdbVotes} </p>
-                       
-                        
+                        </div>
                 </div>
-                </div>                
                 </div>
                 </div>
             </div>
